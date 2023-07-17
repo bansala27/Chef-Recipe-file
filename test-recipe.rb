642a151a5812db5aeb641bead2a21077230128ac
Aseem Bansal <root@Aseems-MacBook-Pro.local>
2023-07-17 14:28:34 +0530

    Updated Code - Install Tree Package

--- test-recipe.rb
+++ test-recipe.rb
@@ -1,4 +1,10 @@
+#code to create a new file in root direectory with data in it
 file '/myFile' do
 content 'Welcome to my chef repository'
 action :create
+end
+
+#code to install tree package in Linux
+package 'tree' do
+action :install
 end
